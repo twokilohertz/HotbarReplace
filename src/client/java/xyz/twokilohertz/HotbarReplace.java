051b73f50361593a39c94b7d748440fbeb745275
termilu <47648082+termilu@users.noreply.github.com>
2024-08-10 19:47:49 +0200

    Added functionality for Minecart replacement

--- src/client/java/xyz/twokilohertz/HotbarReplace.java
+++ src/client/java/xyz/twokilohertz/HotbarReplace.java
@@ -6,6 +6,7 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemPlacementContext;
+import net.minecraft.item.ItemUsageContext;
 import net.minecraft.screen.slot.SlotActionType;
 import net.minecraft.util.Hand;
 import org.lwjgl.glfw.GLFW;
@@ -15,7 +16,6 @@ import org.slf4j.LoggerFactory;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
-
 
 public class HotbarReplace implements ClientModInitializer {
     public static final Logger LOGGER = LoggerFactory.getLogger("hotbarreplace");
@@ -27,32 +27,39 @@ public class HotbarReplace implements ClientModInitializer {
         LOGGER.info("HotbarReplace v0.1.3 initialised");
     }
 
-    public static void tryReplaceSlot(ItemPlacementContext context, Item item, Hand hand) {
-        // Return immediately if player is a spectator
+    public static void tryReplaceSlot(ItemUsageContext context, Item item, Hand hand) {
+        // Return immediately if player is spectator or in creative
         PlayerEntity player = context.getPlayer();
-        if (player.isSpectator())
+        if(player.isSpectator() || player.getAbilities().creativeMode)
             return;
 
-        // Creative inventories don't run out of blocks anyway
-        if (player.getAbilities().creativeMode)
-            return;
-
-        // Get reference to player's current inventory
+        // Return immediately if inventory is empty, null or when current screen handler is null
         PlayerInventory inventory = player.getInventory();
-        if (inventory == null)
+        if(inventory == null || inventory.isEmpty() || player.currentScreenHandler == null)
             return;
 
-        // Return if the inventory is empty
-        if (inventory.isEmpty())
+        //Attempt to find a stack of matching items in the player's inventory
+        ReplaceSlot(item, hand, player, inventory);
+    }
+
+    public static void tryReplaceSlot(ItemPlacementContext context, Item item, Hand hand) {
+        // Return immediately if player is spectator or in creative
+        PlayerEntity player = context.getPlayer();
+        if(player.isSpectator() || player.getAbilities().creativeMode)
             return;
 
-        // If current screen handler is null, return
-        if (player.currentScreenHandler == null)
+        // Return immediately if inventory is empty, null or when current screen handler is null
+        PlayerInventory inventory = player.getInventory();
+        if(inventory == null || inventory.isEmpty() || player.currentScreenHandler == null)
             return;
 
         // Attempt to find a stack of matching items in the player's inventory
+        ReplaceSlot(item, hand, player, inventory);
+    }
+
+    private static void ReplaceSlot(Item lastPlacedItem, Hand hand, PlayerEntity player, PlayerInventory inventory) {
         for (int i = 0; i < player.currentScreenHandler.slots.size(); i++) {
-            if (player.currentScreenHandler.slots.get(i).getStack().isOf(item)) {
+            if (player.currentScreenHandler.slots.get(i).getStack().isOf(lastPlacedItem)) {
                 // Simulate moving the stack from one slot to another
                 if (client != null) {
                     // TODO: This still feels like a bit of a hack
@@ -65,12 +72,7 @@ public class HotbarReplace implements ClientModInitializer {
                     client.interactionManager.clickSlot(player.currentScreenHandler.syncId, i, GLFW.GLFW_MOUSE_BUTTON_1,
                             SlotActionType.PICKUP, player);
 
-                    int slot;
-                    if (hand == Hand.OFF_HAND) {
-                        slot = 9;
-                    } else {
-                        slot = inventory.selectedSlot;
-                    }
+                    int slot = (hand == Hand.OFF_HAND ? 9 : inventory.selectedSlot);
 
                     scheduler.schedule(() -> {
                         client.interactionManager.clickSlot(player.currentScreenHandler.syncId,
